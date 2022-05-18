d6c61218faff30b1d814cf0d3e6c2fbfd4b03b4c
Asma-El-kissi <asmakissi@gmail.com>
2022-05-18 10:58:54 +0200

    checkpoint express

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const userRoute=require('./Routers/userRoute')
-app.use('/api',userRoute)
 const authMiddleware = (req, res, next)=>{
     const hour= new Date().getHours();
     const day = new Date().getDay();
     if ((day > 0 && day < 6) && (hour >= 9 && (hour) <= 17)){
         console.log("app is open 🕖");
-        res.sendFile(__dirname + '/pages');
+        // res.sendFile(__dirname + '/pages/Home Page.html');
         next()
     } else {
         console.log("app error ⛔");
         res.send('We are not working');
-        res.sendFile(__dirname + '/Close.html');
+        // res.sendFile(__dirname + '/Close.html');
         next()
     }
-        // console.log(day)
-        // console.log(hour)
-    }
-    
-    app.use(authMiddleware)
+    // console.log(day)
+    // console.log(hour)
+}
 
-    app.use(express.static(__dirname + '/pages'));
+app.use(authMiddleware)
+app.use('/',userRoute)
+
+app.use(express.static(__dirname + '/pages'));
 
 app.listen(port, (err)=> err ? console.error (err):console.log('The server is running ' , port));
